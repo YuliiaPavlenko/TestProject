4165df8ffdee2f665dab2583678231c67c1f8c34
Yuliia Pavlenko <>
2020-06-20 21:50:04 +0200

    Add companies list vc

---
+++ TestProject/Controllers/CompaniesListViewController.swift
@@ -0,0 +1,105 @@
+//
+//  CompaniesListViewController.swift
+//  TestProject
+//
+//  Created by Yuliia Pavlenko on 20/06/2020.
+//  Copyright © 2020 Yuliia Pavlenko. All rights reserved.
+//
+
+import UIKit
+
+class CompaniesListViewController: UIViewController {
+    private enum CellIdentifiers {
+        static let list = "CompanyList"
+    }
+    
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var indicatorView: UIActivityIndicatorView!
+    
+    private var viewModel: CompaniesViewModel!
+    
+    private var shouldShowLoadingCell = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        indicatorView.startAnimating()
+        indicatorView.isHidden = false
+        
+        tableView.isHidden = true
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.prefetchDataSource = self
+        
+        viewModel = CompaniesViewModel(delegate: self)
+        
+        viewModel.fetchModerators()
+    }
+    
+}
+
+extension CompaniesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.totalCount
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifiers.list, for: indexPath) as! CompanyTableViewCell
+        
+        if isLoadingCell(for: indexPath) {
+            cell.configure(with: .none)
+        } else {
+            cell.configure(with: viewModel.company(at: indexPath.row))
+        }
+        return cell
+    }
+}
+
+extension CompaniesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+}
+
+extension CompaniesListViewController: UITableViewDataSourcePrefetching {
+    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        if indexPaths.contains(where: isLoadingCell) {
+            viewModel.fetchModerators()
+        }
+    }
+}
+
+extension CompaniesListViewController: CompaniesViewModelDelegate {
+    func onFetchCompleted(with newIndexPathsToReload: [IndexPath]?) {
+        
+        guard let newIndexPathsToReload = newIndexPathsToReload else {
+            indicatorView.stopAnimating()
+            indicatorView.isHidden = true
+            tableView.isHidden = false
+            tableView.reloadData()
+            return
+        }
+       
+        let indexPathsToReload = visibleIndexPathsToReload(intersecting: newIndexPathsToReload)
+        tableView.reloadRows(at: indexPathsToReload, with: .automatic)
+    }
+    
+    func onFetchFailed(with reason: String) {
+        indicatorView.stopAnimating()
+        indicatorView.isHidden = true
+        Alert.showAlert(on: self, with: "Warning", message: reason)
+    }
+}
+
+private extension CompaniesListViewController {
+    
+    func isLoadingCell(for indexPath: IndexPath) -> Bool {
+        return indexPath.row >= viewModel.currentCount
+    }
+    
+    func visibleIndexPathsToReload(intersecting indexPaths: [IndexPath]) -> [IndexPath] {
+        let indexPathsForVisibleRows = tableView.indexPathsForVisibleRows ?? []
+        let indexPathsIntersection = Set(indexPathsForVisibleRows).intersection(indexPaths)
+        return Array(indexPathsIntersection)
+    }
+}
