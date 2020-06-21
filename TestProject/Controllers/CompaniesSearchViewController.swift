1d3696fd6e677b4ad519236e9f7ef0844a079bbb
Yuliia Pavlenko <>
2020-06-21 17:25:59 +0200

    Add search company vc

--- TestProject/Controllers/CompaniesSearchViewController.swift
+++ TestProject/Controllers/CompaniesSearchViewController.swift
@@ -6,4 +6,46 @@
 //  Copyright © 2020 Yuliia Pavlenko. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class CompaniesSearchViewController: UIViewController {
+    private enum SegueIdentifiers {
+        static let list = "CompaniesViewController"
+    }
+    
+    @IBOutlet var companyTextField: UITextField!
+    @IBOutlet var searchButton: UIButton!
+    
+    private var behavior: ButtonEnablingBehavior!
+    
+    // MARK: - View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        view.backgroundColor = .white
+        
+        behavior = ButtonEnablingBehavior(textFields: [companyTextField]) { [unowned self] enable in
+            if enable {
+                self.searchButton.isEnabled = true
+                self.searchButton.alpha = 1
+            } else {
+                self.searchButton.isEnabled = false
+                self.searchButton.alpha = 0.7
+            }
+        }
+        
+        companyTextField.setBottomBorder()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == SegueIdentifiers.list {
+            if let listViewController = segue.destination as? FoundCompaniesViewController {
+                listViewController.company = companyTextField.text!
+            }
+        }
+    }
+}
