c8a33d525b209f1e1216d6a0b816d643de590690
Yuliia Pavlenko <>
2020-06-21 17:25:04 +0200

    Add found companies view model

--- TestProject/ViewModels/FoundCompaniesViewModel.swift
+++ TestProject/ViewModels/FoundCompaniesViewModel.swift
@@ -7,3 +7,62 @@
 //
 
 import Foundation
+
+protocol FoundCompaniesViewModelDelegate: class {
+    func onFetchFailed(with reason: String)
+    func showCompanyDetails()
+    func onFetchCompleted()
+    func onFetchCompletedWithNoData()
+}
+
+class FoundCompaniesViewModel {
+    private weak var delegate: FoundCompaniesViewModelDelegate?
+    
+    private var companies: [Company] = []
+    
+    init(delegate: FoundCompaniesViewModelDelegate) {
+        self.delegate = delegate
+    }
+    
+    var companiesCount: Int {
+        return companies.count
+    }
+    
+    
+    func company(at index: Int) -> Company {
+        return companies[index]
+    }
+    
+    func fetchCompanies(with text: String) {
+        
+        NetworkManager.shared.findCompanies(for: text) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    self.delegate?.onFetchFailed(with: error.reason)
+                }
+            case .success(let response):
+                DispatchQueue.main.async {
+                    self.companies.append(contentsOf: response)
+                    if response.count > 0 {
+                        self.delegate?.onFetchCompleted()
+                    } else {
+                        self.delegate?.onFetchCompletedWithNoData()
+                    }
+                    
+                    
+                }
+            }
+        }
+    }
+
+    func companyClicked(_ atIndex: Int) {
+        Cache.shared.setSelectedCompany(companies[atIndex])
+        delegate?.showCompanyDetails()
+    }
+    
+    func setNavigationBarTitle() -> String {
+        return "Companies"
+    }
+}
