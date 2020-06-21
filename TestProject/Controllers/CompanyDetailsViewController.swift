bd944657296981133fc675be960e3e3fc4dd0f60
Yuliia Pavlenko <>
2020-06-21 10:33:58 +0200

    Add company details vc

---
+++ TestProject/Controllers/CompanyDetailsViewController.swift
@@ -0,0 +1,75 @@
+//
+//  CompanyDetailsViewController.swift
+//  TestProject
+//
+//  Created by Yuliia Pavlenko on 20/06/2020.
+//  Copyright © 2020 Yuliia Pavlenko. All rights reserved.
+//
+
+import UIKit
+import Kingfisher
+
+class CompanyDetailsViewController: UIViewController {
+    
+    @IBOutlet var companyNameLabel: UILabel!
+    @IBOutlet var companyDescriptionLabel: UILabel!
+    @IBOutlet var companyAddressLabel: UILabel!
+    @IBOutlet var companyImageView: UIImageView!
+    @IBOutlet var indicatorView: UIActivityIndicatorView!
+    
+    private var viewModel: CompanyDetailsViewModel!
+    
+    // MARK: - View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        indicatorView.startAnimating()
+        indicatorView.isHidden = false
+        
+        viewModel = CompanyDetailsViewModel(delegate: self)
+        
+        viewModel.viewIsPrepared()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        customizeNavigationBar(animated)
+    }
+    
+    // MARK: - Navigation Bar
+    func customizeNavigationBar(_ animated: Bool) {
+        title = viewModel.setNavigationBarTitle()
+    }
+
+}
+
+extension CompanyDetailsViewController: CompanyDetailsViewModelDelegate {
+    func presentAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        Alert.showAlertOnMainThread(on: self, with: title, message: message)
+        
+    }
+    
+    func showCompanyDetails(for company: Company) {
+        DispatchQueue.main.async {
+            self.indicatorView.stopAnimating()
+            self.indicatorView.isHidden = true
+            
+            self.companyNameLabel.text = company.name ?? "No company name"
+            self.companyDescriptionLabel.text = company.description ?? "No company description"
+            self.companyAddressLabel.text = (company.address?.city)!
+                //+ company.address?.coordinates?.latitude ?? "" + company.address?.coordinates?.longitude ?? ""
+            
+            if let coverImage = company.coverImage {
+                let fixedCoverImageName = coverImage.replacingOccurrences(of: "i.", with: "")
+                let url = URL(string: fixedCoverImageName)
+                self.companyImageView.kf.setImage(with: url)
+            } else {
+                self.companyImageView.image = UIImage(named: "noImageIcon.png")
+            }
+        }
+        
+        
+        
+    }
+    
+    
+}
