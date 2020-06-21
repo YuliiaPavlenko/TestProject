58ece4f87000dabb1a44ef5c920fc698a4acce59
Yuliia Pavlenko <>
2020-06-21 17:13:10 +0200

    Add search icon

---
+++ TestProject/Controllers/CompaniesSearchViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CompaniesSearchViewController.swift
+//  TestProject
+//
+//  Created by Yuliia Pavlenko on 21/06/2020.
+//  Copyright © 2020 Yuliia Pavlenko. All rights reserved.
+//
+
+import Foundation
