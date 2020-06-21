b764f3b0d71b44aece2c0187abd2b6200c4382fe
Yuliia Pavlenko <>
2020-06-21 17:22:32 +0200

    Set Bottom Border to text filed

--- TestProject/Extensions/UITextField+Extensions.swift
+++ TestProject/Extensions/UITextField+Extensions.swift
@@ -6,4 +6,18 @@
 //  Copyright © 2020 Yuliia Pavlenko. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UITextField {
+  func setBottomBorder() {
+    borderStyle = .none
+    layer.backgroundColor = UIColor.white.cgColor
+    
+    layer.masksToBounds = false
+    
+    layer.shadowColor = UIColor.lightGray.cgColor
+    layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+    layer.shadowOpacity = 1.0
+    layer.shadowRadius = 0.0
+  }
+}
