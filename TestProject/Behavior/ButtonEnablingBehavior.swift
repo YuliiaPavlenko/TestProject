9e180284fd22e86416c3b8200eaec221b8fa22c3
Yuliia Pavlenko <>
2020-06-21 17:13:43 +0200

    Add search button behaviour

--- TestProject/Behavior/ButtonEnablingBehavior.swift
+++ TestProject/Behavior/ButtonEnablingBehavior.swift
@@ -6,4 +6,35 @@
 //  Copyright © 2020 Yuliia Pavlenko. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+final class ButtonEnablingBehavior: NSObject {
+    let textFields: [UITextField]
+    
+    let onChange: (Bool) -> Void
+    
+    init(textFields: [UITextField], onChange: @escaping (Bool) -> Void) {
+        self.textFields = textFields
+        self.onChange = onChange
+        super.init()
+        setup()
+    }
+    
+    private func setup() {
+        textFields.forEach { textField in
+            textField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        }
+        onChange(false)
+    }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        var enable = true
+        for textField in textFields {
+            guard let text = textField.text, !text.isEmpty else {
+                enable = false
+                break
+            }
+        }
+        onChange(enable)
+    }
+}
